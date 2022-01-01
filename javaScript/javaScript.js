06182ed4c724362def6a864dbe94bc34a95f8324
Samir Santos <santossamir@hotmail.com>
2022-01-01 15:02:36 -0300

    Adicionando a submitingForm() e a AddingTransactions().

--- javaScript/javaScript.js
+++ javaScript/javaScript.js
@@ -1,7 +1,20 @@
-//Início da função toClean - Limpar Dados. ---------------------------
-function toClean(){
+const writingFormatter = new Intl.NumberFormat('pt-BR', {
+	style: 'currency',
+	currency: 'BRL',
+	minimumFractionDigits: 2,
+});
 
-}
+var transactions = localStorage.getItem("transactions") ? JSON.parse(localStorage.getItem("transactions")) : [];
+
+//Início da função toClean - Limpar Dados. ---------------------------
+/*function toClean(){
+	let userConfirm = confirm("Deseja remover todas as transações?");
+	if(userConfirm){
+		localStorage.removeItem("transactions");
+		transactions = [];
+		AddingTransactions();
+	}
+}*/
 //Final da função toClean - Limpar Dados. ----------------------------
 
 // Início da função focar no formulário - Cadastro de Transações.----------------------
@@ -37,9 +50,74 @@ function currencyFormat(moeda){
 // Final da escrita padrão do valor moeda. --------------------------------------
 
 // Início da Extrato de transações. ---------------------------------------------
+async function AddingTransactions(){
+	let total = 0;
+
+	for(item in transactions){
+
+		if(transactions[item].transType == "compra"){
+			total -= transactions[item].transCurrency;
+		}else{
+			total += transactions[item].transCurrency;
+		}
+
+		document.querySelector(".tabela-corpo").innerHTML += 
+		`<tr>
+            <td class="tabelaMais-corpo">${transactions[item].transType == "compra" ? "-" : "+"}</td>
+            <td class="tabela-mercadoria">${transactions[item].transName}</td>
+            <td class="tabela-valorMercadoria">${writingFormatter.format(transactions[item].transCurrency.toString().replace(/([0-9]{2})$/g, ".$1"))}</td>
+        </tr>`
+	}
+
+	if(transactions.length > 0){
+		document.querySelector("#tabela-foot").innerHTML += 
+		`<tr>
+            <td class="tabela-total-vazio"></td>
+            <td class="tabela-total">Total</td>
+            <td class="tabela-valorTotal"> ${writingFormatter.format(total.toString().replace(/([0-9]{2})$/g, ".$1"))} <br> <span class="lucro">[${Math.sign(total) == 1 ? "LUCRO" : "PREJUÍZO"}]</span></td>
+        </tr>
+        `
+	}
+}
 
 //Final da Extrato de transações. -----------------------------------------------
 
 // Início do formulário Nova Transação. -----------------------------------------
+function submitingForm(e){
+	e.preventDefault();
+
+	transactionType = document.querySelector('select[name="compra&venda"]');
+	transactionName = document.querySelector('input[name="nomeMercadoria"]');
+	transactionCurrency = document.querySelector('input[name="valorMoeda"]');	
+
+	if(!transactionName.value){
+		transactionName.focus();
+		return;
+	}
+
+	console.log(!transactionCurrency.value && transactionCurrency.value.replace(/[^0-9]/g, "") == "")
+
+	if(transactionCurrency.value.replace(/[^0-9]/g, "") == ""){
+		transactionCurrency.focus();
+		return;
+	}
+
+	currencyNumber = parseInt(transactionCurrency.value.replace(/[^0-9]/g, ""));
+
+	transactions.push({
+		transType: transactionType.value,
+		transName: transactionName.value,
+		transCurrency: currencyNumber  
+	})
+
+	transactionName.value = "";
+	transactionCurrency.value = "";
+
+	localStorage.setItem("transactions", JSON.stringify(transactions));
+
+	AddingTransactions();
+}
+
+AddingTransactions();
 
 // Final do formulário Nova Transação. ------------------------------------------
